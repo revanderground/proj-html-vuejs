a1f4a4131957edea2fc362185be1448cd8a4b379
vtnboolean <vnthuanx@gmail.com>
2022-07-27 18:46:14 +0200

    audiobook in progress

--- src/components/Audiobook.vue
+++ src/components/Audiobook.vue
@@ -4,18 +4,34 @@
         <div class="logos-container">
             
             <div class="logo-store-box px-2">
-                <img class="logo-store" src="../assets/img/ebay-logotan-2x-600x197.png" >
+                <img class="logo-store" src="../assets/img/ebay-logotan-2x-600x197.png" alt="logo-store">
             </div>
              <div class="logo-store-box px-3">
-                <img class="logo-store" src="../assets/img/audible-logotan-2x-600x197.png" >
+                <img class="logo-store" src="../assets/img/audible-logotan-2x-600x197.png" alt="logo-store" >
             </div>
              <div class="logo-store-box px-3">
-                <img class="logo-store" src="../assets/img/bb-logotan-2x-600x197.png" >
+                <img class="logo-store" src="../assets/img/bb-logotan-2x-600x197.png" alt="logo-store" >
             </div>
              <div class="logo-store-box px-3">
-                <img class="logo-store" src="../assets/img/kindlefire-logotan-2x-600x197.png" >
+                <img class="logo-store" src="../assets/img/kindlefire-logotan-2x-600x197.png" alt="logo-store" >
             </div>
 
+        </div>
+
+    </section>
+
+    <section class="banner">
+        <div class="container-elements d-flex">
+            <div class="left-side-banner col-6 text-center">
+                <div class="iphone-box text-center">
+                    <img class="iphone" src="../assets/img/audible-app-2x-600x628.png" alt="iphone"></img>
+                </div>
+                
+            </div>
+
+            <div class="right-side-banner text-center col-6">
+                
+            </div>
         </div>
 
     </section>
