79c5ffb02a087b438ba194f5167f1002d2adab57
vtnboolean <vnthuanx@gmail.com>
2022-07-27 16:22:34 +0200

    online-store logos

---
+++ src/components/Audiobook.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="audiobook-section">
+    <section class="online-stores-logos">
+        <div class="logos-container">
+            
+            <div class="logo-store-box px-2">
+                <img class="logo-store" src="../assets/img/ebay-logotan-2x-600x197.png" >
+            </div>
+             <div class="logo-store-box px-3">
+                <img class="logo-store" src="../assets/img/audible-logotan-2x-600x197.png" >
+            </div>
+             <div class="logo-store-box px-3">
+                <img class="logo-store" src="../assets/img/bb-logotan-2x-600x197.png" >
+            </div>
+             <div class="logo-store-box px-3">
+                <img class="logo-store" src="../assets/img/kindlefire-logotan-2x-600x197.png" >
+            </div>
+
+        </div>
+
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/Audiobook.scss';
+@import '../styles/utilities.scss'
+
+
+</style>
--- src/components/MoreAbout.vue
+++ src/components/MoreAbout.vue
@@ -1,5 +1,17 @@
 <template>
-  
+    <div class="more-about">
+        
+        <div class="text-center container-more">   
+            <div class="title">More About Damon Vaughn</div>
+            <div class="description py-2">Damon Vaughn is a professor of psychology at the University of Toronto, a clinical psychologist and the author of the multi-million copy bestseller “The story of my life.”</div>
+            
+            <div class="py-4">
+                <div class="my-button py-2">Meet The Author</div>
+            </div>
+        </div>
+
+    </div>
+
 </template>
 
 <script>
@@ -8,6 +20,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../styles/MoreAbout.scss';
+@import '../styles/utilities.scss'
 
 </style>
