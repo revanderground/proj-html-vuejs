7e69fa12289a127c7e445f9ccef1ff1b891f38fc
vtnboolean <vnthuanx@gmail.com>
2022-07-28 17:58:58 +0200

    starting footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,72 @@
 <template>
-  <footer>
-    <div>
-        <h2>Footer</h2>
-    </div>
-  </footer>
+    <header id="header">
+        <div class="container-fluid">
+            <div class="jumbotron">
+                
+                
+
+            </div>
+
+        </div>
+
+    </header>
+  
 </template>
 
 <script>
 export default {
-
+      data: function(){
+        return {
+            navbarElements: [
+                {
+                    text: 'Home',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'About Me',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Testimonials',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'My Blog',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Meetups',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shops',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Contact Me', 
+                    url: '#',
+                    current: false,
+                },
+          
+          
+            ]
+        }
+    }    
 }
+
+
 </script>
 
-<style>
+<style lang="scss" scoped>
+    @import '../styles/viarables.scss';
+    @import '../styles/Footer.scss'
+ 
+    
 
 </style>
